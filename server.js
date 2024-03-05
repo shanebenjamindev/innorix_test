c200b53f1801f5f0cf27c1ed53cf91ff34c29610
Your Name <you@example.com>
2024-03-05 21:56:59 +0700

    added code

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const path = require('path')
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    res.render('../main');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
